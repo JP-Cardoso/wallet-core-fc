1cfb801cb1949871758c3044c69b636660ee3372
JP-Cardoso <jhonpoal@hotmail.com>
2023-08-02 13:56:51 -0300

    feat: creating unit test for create transaction use case

---
+++ internal/usecase/create_transaction/create_transaction_test.go
@@ -0,0 +1,64 @@
+package createtransaction
+
+import (
+	"testing"
+
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type TransactionGatewayMock struct {
+	mock.Mock
+}
+
+func (m *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
+	args := m.Called(transaction)
+	return args.Error(0)
+}
+
+type AccountGatewayMock struct {
+	mock.Mock
+}
+
+func (m *AccountGatewayMock) Save(account *entity.Account) error {
+	args := m.Called(account)
+	return args.Error(0)
+}
+
+func (m *AccountGatewayMock) FindById(id string) (*entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Account), args.Error(1)
+}
+
+func TestCreateTransactionUseCase_Execute(t *testing.T) {
+	client1, _ := entity.NewClient("João", "test@teste.com")
+	account1 := entity.NewAccount(client1)
+	account1.Credit(1000)
+
+	client2, _ := entity.NewClient("John Doe", "testII@teste.com")
+	account2 := entity.NewAccount(client2)
+	account2.Credit(1000)
+
+	mockAccount := &AccountGatewayMock{}
+	mockAccount.On("FindById", account1.ID).Return(account1, nil)
+	mockAccount.On("FindById", account2.ID).Return(account2, nil)
+
+	mockTransaction := &TransactionGatewayMock{}
+	mockTransaction.On("Create", mock.Anything).Return(nil) //Que ela não retorne um erro
+
+	inputDto := CreateTransactionInputDto{
+		AccountIDFrom: account1.ID,
+		AccountIDTo:   account2.ID,
+		Amount:        100,
+	}
+
+	uc := NewCreateTransactionUseCase(mockTransaction, mockAccount)
+	output, err := uc.Execute(inputDto)
+	assert.Nil(t, err) //Meu erro tem que ser em branco
+	assert.NotNil(t, output)
+	mockAccount.AssertExpectations(t)
+	mockAccount.AssertNumberOfCalls(t, "FindById", 2)
+	mockTransaction.AssertExpectations(t)
+	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+}
